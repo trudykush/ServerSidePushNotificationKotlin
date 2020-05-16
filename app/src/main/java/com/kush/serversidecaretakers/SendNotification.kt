a223fb1e07bf0ca2644c3842722418999fdff145
Kushal.Mishra <kushalmishra91@gmail.com>
2020-05-16 15:02:33 +0100

    Customizing the notification

--- app/src/main/java/com/kush/serversidecaretakers/SendNotification.kt
+++ app/src/main/java/com/kush/serversidecaretakers/SendNotification.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Response
 import com.android.volley.VolleyError
@@ -19,12 +21,20 @@ class SendNotification : AppCompatActivity() {
     private val privateKeyObject = PrivateKeys()
     val privateServerKey: String? = privateKeyObject.applicationServerKey
 
+    private lateinit var messageID: EditText
+    private lateinit var titleMessage: EditText
+    private lateinit var message: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_send_notification)
+
         val sendNotification = sendPushNotification
         sendNotification.setOnClickListener { view: View? -> sendUsingVolley() }
+
+        messageID = MessageIDET as EditText
+        titleMessage = titleET
+        message = MessageET
     }
 
     private fun sendUsingVolley() {
@@ -33,9 +43,9 @@ class SendNotification : AppCompatActivity() {
         try {
             json.put("to", "/topics/" + "Kush")
             val dataObject = JSONObject()
-            dataObject.put("Title", "Testing")
-            dataObject.put("Message", "Push Notification")
-            dataObject.put("NotificationID", "1")
+            dataObject.put("Title", titleMessage.text.toString())
+            dataObject.put("Message", message.text.toString())
+            dataObject.put("NotificationID", messageID.text.toString())
             json.put("data", dataObject)
             val URL = "https://fcm.googleapis.com/fcm/send"
             val request: JsonObjectRequest = object : JsonObjectRequest(Method.POST, URL,
